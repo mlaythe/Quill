be3fc5e73b18b282aa1351245c1225c93fbd7227
Michael Laythe <mrlaythe24@aol.com>
2017-01-10 23:28:46 -0800

    add validation, especially email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
