044f4c4db36b20770cfe44274315cc6a42113614
Michael Laythe <mrlaythe24@aol.com>
2017-01-11 13:14:35 -0800

    add secure password and email edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { minimum: 5 }
   validates :password, presence: true, length: { minimum: 5 }
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
+  has_secure_password
 end
