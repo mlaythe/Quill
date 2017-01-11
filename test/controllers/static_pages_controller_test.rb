d4e2f104f6ffbc7685b6582d06629f06acdbb35f
Michael Laythe <mrlaythe24@aol.com>
2017-01-11 11:39:10 -0800

    add integration test for root path to check links to different views

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,6 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get '/'
     assert_response :success
+    assert_select "h1", "Home"
   end
 
 end
